eb68fc45069fbe63dae6a704f3da9417fbccf60b
Nicholas mendes <mendes.nik@hotmail.com>
2022-04-21 19:28:59 -0300

    Quarto Commit

--- JavaScript.js
+++ JavaScript.js
@@ -30,3 +30,45 @@ function LeiaMais() {
         btnLeiaMais.innerHTML = "Leia Mais"
     }
 }
+
+function criarTopico() {
+    let btnCriarTopico = document.getElementById("btnCriarTopico");
+    var formulario = document.getElementById("formulario");
+
+    if( formulario.style.display === "none") {
+        formulario.style.display = "inline"
+        btnCriarTopico.innerHTML = "Criar Outro"
+    }else {
+        formulario.style.display = "none"
+        btnCriarTopico.innerHTML = "criar tópico"
+
+    }
+
+    
+}
+
+function enviarResposta() {
+    var buttonenviar = document.getElementById("button-enviar");
+    var formulario = document.getElementById("formulario");
+    var textarea = document.getElementById("exampleFormControlTextarea1");
+    var paragrafodestaque = document.getElementById("paragrafo-destaque");
+    var subparagrafo = document.getElementById("subparagrafo");
+    var questionamentos = document.getElementById("questionamentos");
+    var feedbackeditar = document.getElementById("feedback-editar");
+
+    
+    if( textarea.value !== " ") {
+        formulario.style.display = "none"
+        alert("Enviado!")
+        paragrafodestaque.innerHTML = "Seu tópico foi enviado com sucesso! :D"
+        subparagrafo.innerHTML = "Agradecemos por sua contribuição, uma notificação será enviada ao seu email assim que seu tópico for respondido!"
+        feedbackeditar.style.display = "inline"
+        
+    }else {
+        alert("Preencha o campo!")
+    }
+    
+
+
+
+}
